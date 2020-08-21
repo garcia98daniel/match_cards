2ec16c9a5b44a89c5279b4ecb605c6a184c3e6c2
garcia98daniel <garcia98daniel@gmail.com>
2020-08-21 12:22:56 -0500

    frente y espalda de cartas realizada

---
+++ gameCuple.js
@@ -0,0 +1,9 @@
+// function voltear_tarjeta(){
+    const carta = document.querySelector(".game_section");
+    console.log(carta)
+
+    carta.style.backgroundColor="blue";
+// }
+
+// voltear_tarjeta();
+
